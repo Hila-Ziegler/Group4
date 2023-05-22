accf9566ec5690893fd5fea131f1109d745d0e8a
AviachenCohen <aviacoheen@gmail.com>
2023-05-22 12:47:49 +0300

    Changed label name in form 1

--- Group4/Form1.Designer.cs
+++ Group4/Form1.Designer.cs
@@ -38,9 +38,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(274, 94);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(202, 80);
+            this.label1.Size = new System.Drawing.Size(144, 80);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "avia";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
