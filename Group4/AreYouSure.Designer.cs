529d639c2bbad8e8856392848f6947366d1ed8ee
Ofirw12 <ofir12121@gmail.com>
2023-06-05 15:30:05 +0300

    BookCrud functionality complete

--- Group4/AreYouSure.Designer.cs
+++ Group4/AreYouSure.Designer.cs
@@ -37,39 +37,45 @@ namespace Group4
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(116, 38);
+            this.label1.Location = new System.Drawing.Point(155, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(234, 13);
+            this.label1.Size = new System.Drawing.Size(307, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Are You Sure You Want To DELETE This Item?";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(257, 93);
+            this.button1.Location = new System.Drawing.Point(343, 114);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
             this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Delete";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(131, 93);
+            this.button2.Location = new System.Drawing.Point(175, 114);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 2;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // AreYouSure
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 149);
+            this.ClientSize = new System.Drawing.Size(649, 183);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AreYouSure";
             this.Text = "AreYouSure";
             this.ResumeLayout(false);
