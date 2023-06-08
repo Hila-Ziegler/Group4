1d5942e8bb8528fec80d866bcc1362cb32c42b4d
Ofirw12 <ofir12121@gmail.com>
2023-06-08 19:36:39 +0300

    Create new ReadingGoalReport

---
+++ Group4/ReadingGoals.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group4
+{
+    public partial class ReadingGoals : Form
+    {
+        Student s;
+        public ReadingGoals(Student s)
+        {
+            this.s = s;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadingGoalReport rgr = new ReadingGoalReport(this.s);
+            rgr.Show();
+            this.Hide();
+        }
+
+        private void setGoalbtn_Click(object sender, EventArgs e)
+        {
+            this.s.set_yearlyGoal(int.Parse(numericYearlyGoal.Value.ToString()));
+            this.s.update_Student();
+        }
+
+        private void ReadingGoals_Load(object sender, EventArgs e)
+        {
+            if (this.s.get_yearlyGoal() < 10)
+            {
+                numericYearlyGoal.Value = 2;
+            }
+            else numericYearlyGoal.Value = 20;
+        }
+    }
+}
