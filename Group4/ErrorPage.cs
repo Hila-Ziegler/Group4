5a7d99ad5a9666838db72c23a5734542f316e523
IvannaMaluta <ivannamaluta@gmail.com>
2023-06-13 18:15:47 +0300

    updated wrong input in copy crud

---
+++ Group4/ErrorPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group4
+{
+    public partial class ErrorPage : Form
+    {
+        String message;
+
+        public ErrorPage(String m)
+        {
+            this.message = m;
+            InitializeComponent();
+        }
+
+        private void ErrorPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Accept_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
